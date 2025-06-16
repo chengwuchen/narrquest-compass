13417c02e1c448c017ef20aba889aa555f6ba562
chengwuchen <chengwuchen@nkust.edu.tw>
2025-06-16 21:33:47 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NarrQuestCompass from "./pages/index.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NarrQuestCompass />
+  </React.StrictMode>
+);
